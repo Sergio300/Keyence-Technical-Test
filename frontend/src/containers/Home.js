a5106035261b3a03488c5371e633df3dc08f6850
Sergio300 <dsergio300@gmail.com>
2023-04-17 12:10:15 -0600

    Keyence

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -27,20 +27,33 @@ export function Home() {
     window.location.reload()
   }
 
-  if (post.length === 0) {
-    return <div>
-      <VscEmptyWindow/>
-      <h1>NO DATA</h1>
-      <div className='flex justify-center px-3 py-2'>
-        <input type='file' name="file" className='px-3 py-2 focus:outline-none rounded bg-gray-600 text-white mb-4 m-4'
-        onChange={(e)=>upload(e.target.files[0])}
-        ></input>
-        <button className='px-3 py-2 focus:outline-none rounded bg-gray-800 text-white mb-4 m-4' 
-          onClick={()=>insertFile()}
-          
-        >Upload</button>
+  
+
+  const rendermain = () =>{
+
+    if (post.length === 0) {
+      return <div>
+        <VscEmptyWindow/>
+        <h1>NO DATA</h1>
+        <div className='flex justify-center px-3 py-2'>
+          <input type='file' name="file" className='px-3 py-2 focus:outline-none rounded bg-gray-600 text-white mb-4 m-4'
+          onChange={(e)=>upload(e.target.files[0])}
+          ></input>
+          <button className='px-3 py-2 focus:outline-none rounded bg-gray-800 text-white mb-4 m-4' 
+            onClick={()=>insertFile()}
+            
+          >Upload</button>
+        </div>
       </div>
-    </div>
+    }
+    else{
+      <div className='grid md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-3 '>
+        {post.map(post =>(
+          <Card post={post} key={post._id}></Card>
+        ))}
+      </div>
+    }
+
   }
 
   return (
@@ -60,12 +73,8 @@ export function Home() {
             Create New Data
           </button>
       </Link>
-
-      <div className='grid md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-3 '>
-        {post.map(post =>(
-          <Card post={post} key={post._id}></Card>
-        ))}
-      </div>
+      
+      {rendermain()}
       
     </div>
 
