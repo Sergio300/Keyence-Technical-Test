5d536e2a5297a6f2aee4ecae51077954283d3063
Sergio300 <dsergio300@gmail.com>
2023-04-17 14:56:05 -0600

    Keyence

--- Backend/config.js
+++ Backend/config.js
@@ -2,5 +2,5 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const mongoDB = process.env.MONGODB || "mongodb://localhost/keyencemongodb"
+export const mongoDB = process.env.MONGODB || "mongodb://localhost:3000/keyencemongodb"
 export const PORT = process.env.PORT || 3000
