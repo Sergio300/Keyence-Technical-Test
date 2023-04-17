5c98d0ba3d8877b03910d7ef799d5fc15eed047f
Sergio300 <dsergio300@gmail.com>
2023-04-17 14:50:13 -0600

    Keyence

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom'
 import {VscEmptyWindow} from 'react-icons/vsc'
 import {Card} from '../components/Card'
 import axios from 'axios';
-import { response } from 'express';
 
 export function Home() {
 
@@ -19,13 +18,12 @@ export function Home() {
   const insertFile = async () => {
     const f = new FormData()
     f.append("file",archivo)
-    await axios.post('/importExcel', f,{headers:{'Content-Type': 'multipart/form-data'}})
+    await axios.post('http://localhost:3000/importExcel', f,{headers:{'Content-Type': 'multipart/form-data'}})
     .then(response=>{
       console.log(response.data)
     }).catch(error=>{
       console.log(error)
     })
-    console.log(response.data)
     window.location.reload()
   }
 
